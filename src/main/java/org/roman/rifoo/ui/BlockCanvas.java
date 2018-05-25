0b7b726f5d31517c287c9f24bb0bb05c9ed9cd43
RMuskovets <rmuskovets@gmail.com>
2018-05-25 14:52:14 +0300

    Added UI component

---
+++ src/main/java/org/roman/rifoo/ui/BlockCanvas.java
@@ -0,0 +1,45 @@
+package org.roman.rifoo.ui;
+
+import lombok.Data;
+
+import javax.swing.*;
+import java.awt.*;
+
+@Data
+public class BlockCanvas extends JPanel {
+
+    private Color[][] MATRIX = new Color[10][10];
+    private final int gridWidth, gridHeight;
+    private int gridSize = 10;
+    private boolean showBorder = true;
+
+    public void setColor(int x, int y, Color c) {
+        MATRIX[y][x] = c;
+    }
+
+    public Color getColor(int x, int y) {
+        return MATRIX[y][x];
+    }
+
+    public BlockCanvas(int width, int height) {
+        MATRIX = new Color[height][width];
+        this.gridHeight = height;
+        this.gridWidth = width;
+    }
+
+    public BlockCanvas(int sideWidth) {
+        this(sideWidth, sideWidth);
+    }
+
+    public void paintComponent(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        for (int i = 0; i < gridWidth; i++)
+            for (int j = 0; j < gridHeight; j++) {
+                int x = i * gridSize;
+                int y = j * gridSize;
+                g.setColor(MATRIX[y][x]);
+                g.fillRect(x, y, gridSize, gridSize);
+            }
+    }
+}
---
+++ src/test/java/org/roman/rifoo/TestBCanvas.java
@@ -0,0 +1,22 @@
+package org.roman.rifoo;
+
+import org.junit.Test;
+import org.roman.rifoo.ui.BlockCanvas;
+
+import java.awt.*;
+
+public class TestBCanvas {
+
+    @Test
+    public void testBlockCanvas() {
+        BlockCanvas cvs = new BlockCanvas(10);
+        cvs.equals(null);
+        cvs.getGridHeight();
+        cvs.getGridSize();
+        cvs.getGridWidth();
+        cvs.getMATRIX();
+        cvs.isShowBorder();
+        cvs.setColor(0,0, Color.BLACK);
+        cvs.getColor(1,1);
+    }
+}
